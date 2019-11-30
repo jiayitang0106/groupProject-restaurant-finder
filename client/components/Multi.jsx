b3e6285fe80bd8d701430eaa9c469896da85a1c2
Allen Tsai <hello.allen.tsai@gmail.com>
2019-11-30 15:45:59 -0800

    added styled icons

--- client/components/Multi.jsx
+++ client/components/Multi.jsx
@@ -1,5 +1,134 @@
 import React from 'react';
 import styled from 'styled-components';
+import { CloseCircle } from 'styled-icons/remix-line/CloseCircle';
+
+const StyledContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+  background-color: rgba(16,24,32,.95);
+  box-sizing: border-box;
+  padding: 0 40px 40px;
+  text-align: center;
+  color: #fff;
+  /* display: table; */
+`
+const StyledGridWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  vertical-align: middle;
+`
+
+const StyledGrid = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  width: 1000px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 5px;
+  background: none;
+  border-radius: 0;
+  box-shadow: none;
+  max-height: 850px;
+  max-width: 100%;
+  overflow-y: scroll;
+
+  &::-webkit-slider-thumb {
+    background-color: #e1e1e1;
+    background: #e1e1e1;
+    color: #e1e1e1;
+    border-radius: 10px;
+  }
+
+`
+
+const StyledImgContainer = styled.div`
+  display: inline-block;
+  overflow: hidden;
+  background-color: black;
+`
+
+const StyledImg = styled.img`
+  width: 100%;
+  height: 250px;
+  object-fit: cover;
+  transition: .2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.01);
+    opacity: .9;
+  }
+`
+
+const StyledNavWrapper = styled.div`
+  vertical-align: middle;
+  display: flex;
+  flex-flow: row wrap;
+  top: 0px;
+  height: 80px;
+  width: 100%;
+`
+
+const StyledNav = styled.div`
+  margin: auto;
+  vertical-align: middle;
+  color: white;
+  font: 11px/13px 'Arial';
+  letter-spacing: .2em;
+  text-transform: uppercase;
+  flex: 1;
+`
+
+const StyledCounter = styled.span`
+  padding-right: 10px;
+  visibility: hidden;
+`
+
+const StyledClosed = styled(CloseCircle)`
+  padding: 8px;
+  color: #e1e1e1;
+  &:hover {
+    color: #101820;
+    background-color: #e1e1e1;
+    opacity: .85;
+  }
+`
+
+const Multi = ({imageUrls, handleClick, handleView}) =>  {
+  return (
+    <StyledContainer>
+      <StyledNavWrapper>
+        <StyledNav>
+        </StyledNav>
+        <StyledNav>
+          Barndiva
+        </StyledNav>
+        <StyledNav>
+          <StyledCounter>
+            0 of 0
+            <button onClick={() => handleView('multi')}>Multi</button> |
+          </StyledCounter>
+          <StyledClosed size="22" onClick={() => handleView('main')}>close</StyledClosed>
+        </StyledNav>
+      </StyledNavWrapper>
+      <StyledGridWrapper>
+        <StyledGrid>
+          {imageUrls.map((url, idx) => <StyledImgContainer key={idx}><StyledImg src={url} key={idx} onClick={() => handleClick(idx)}></StyledImg></StyledImgContainer>)}
+        </StyledGrid>
+      </StyledGridWrapper>
+    </StyledContainer>
+  );
+}
+
+export default Multi
+
+/*
+
+import React from 'react';
+import styled from 'styled-components';
 
 const StyledContainer = styled.div`
   position: absolute;
@@ -72,7 +201,6 @@ const Multi = ({imageUrls, handleClick, handleView}) =>  {
         <StyledNav>
           <StyledName>Barndiva</StyledName>
           <StyledButtonWrapper>
-
             <button onClick={() => handleView('main')}>close</button>
           </StyledButtonWrapper>
         </StyledNav>
@@ -84,3 +212,4 @@ const Multi = ({imageUrls, handleClick, handleView}) =>  {
 }
 
 export default Multi
+*/
--- client/components/Single.jsx
+++ client/components/Single.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import { CloseCircle } from 'styled-icons/remix-line/CloseCircle';
+import { Grid } from 'styled-icons/boxicons-solid/Grid';
+import { KeyboardArrowRight } from 'styled-icons/material/KeyboardArrowRight';
+import { KeyboardArrowLeft } from 'styled-icons/material/KeyboardArrowLeft';
 
 const StyledContainer = styled.div`
   position: absolute;
@@ -11,41 +15,98 @@ const StyledContainer = styled.div`
   background-color: rgba(16,24,32,.95);
   box-sizing: border-box;
   padding: 0 40px 40px;
+  text-align: center;
   /* margin-top: auto;
   margin-bottom: auto; */
-  text-align: center;
-  display: table;
+  /* display: table; */
 `
 
 const StyledImgWrapper = styled.div`
-  display: table-cell;
+  display: flex;
+  flex-direction: column;
   vertical-align: middle;
 `
 
 const StyledImg = styled.img`
   margin-left: auto;
   margin-right: auto;
-  width: 800px
-  max-height: 800px;
+  width: 100%;
+  max-width: 1400px
+  max-height: 900px;
   object-fit: cover;
 `
 
-const StyledNav = styled.div`
-  vertical-align: middle;
-  display: table-row;
-  top: 0px;
-  height: 80px;
-  width: 1400px;
+const StyledButtonWrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  background: none;
+  border: 0;
 `
 
-const StyledName = styled.span`
-  display: table-cell;
+const StyledNavWrapper = styled.div`
+  vertical-align: middle;
+  display: flex;
+  flex-flow: row wrap;
+  top: 0px;
+  height: 80px;
+  width: 100%;
+`
+
+const StyledNav = styled.div`
+  margin: auto;
   vertical-align: middle;
   color: white;
   font: 11px/13px 'Arial';
   letter-spacing: .2em;
   text-transform: uppercase;
-  align: center;
+  flex: 1;
+`
+
+const StyledCounter = styled.span`
+  padding-right: 8px;
+  letter-spacing: .05em;
+`
+
+const StyledClosed = styled(CloseCircle)`
+  padding: 8px;
+  color: #e1e1e1;
+  &:hover {
+    color: #101820;
+    background-color: #e1e1e1;
+    opacity: .85;
+  }
+`
+
+const StyledMulti = styled(Grid)`
+  padding: 8px;
+  color: #e1e1e1;
+  &:hover {
+    color: #101820;
+    background-color: #e1e1e1;
+    opacity: .85;
+  }
+`
+
+const StyledLeft = styled(KeyboardArrowLeft)`
+  padding: 5px;
+  color: #e1e1e1;
+  &:hover {
+    color: #101820;
+    background-color: #e1e1e1;
+    opacity: .8;
+    border-radius: 50%;
+  }
+`
+
+const StyledRight = styled(KeyboardArrowRight)`
+  padding: 5px;
+  color: #e1e1e1;
+  &:hover {
+    color: #101820;
+    background-color: #e1e1e1;
+    opacity: .8;
+    border-radius: 50%;
+  }
 `
 
 const Single = ({ idx, total, handleView, url, handleClick }) =>  {
@@ -68,22 +129,26 @@ const Single = ({ idx, total, handleView, url, handleClick }) =>  {
 
   return (
     <StyledContainer >
-      <StyledNav>
-        <StyledName>Barndiva
-          <div>
-
-        <span>{idx+1} of {total}</span>
-        <button onClick={() => handleView('multi')}>Multi</button> |
-        <button onClick={() => handleView('main')}>Close</button>
-          </div>
-        </StyledName>
-      </StyledNav>
+      <StyledNavWrapper>
+        <StyledNav>
+        </StyledNav>
+        <StyledNav>
+          Barndiva
+        </StyledNav>
+        <StyledNav>
+          <StyledCounter>
+            {idx+1} of {total}
+          </StyledCounter>
+          <StyledMulti size="20" onClick={() => handleView('multi')}>multi</StyledMulti>
+          <StyledClosed size="22" onClick={() => handleView('main')}></StyledClosed>
+        </StyledNav>
+      </StyledNavWrapper>
       <StyledImgWrapper>
         <StyledImg src={url} />
-        <div>
-          <button onClick={() => handleClick(prev)}>Prev</button>
-          <button onClick={() => handleClick(next)}>Next</button>
-        </div>
+        <StyledButtonWrapper>
+          <StyledLeft size="22" onClick={() => handleClick(prev)}>prev</StyledLeft>
+          <StyledRight size="22" onClick={() => handleClick(next)}>next</StyledRight>
+        </StyledButtonWrapper>
       </StyledImgWrapper>
     </StyledContainer>
   );
