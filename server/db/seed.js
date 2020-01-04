e3cbaba88641f9c51a2f116425c2f4594738241f
apple <apple@appletekiair.lan>
2020-01-04 14:53:48 -0800

    cassandra connected to client and server(image urls modified)

--- server/db/seed.js
+++ server/db/seed.js
@@ -2,7 +2,7 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const faker = require('faker');
 
 const csvWriter = createCsvWriter({
-  path:'images.csv',
+  path:'images5.csv',
   header: [
     {id:'id', title:'id'},
     {id:'name', title: 'names'},
@@ -12,7 +12,7 @@ const csvWriter = createCsvWriter({
 
 const records = [];
 const seed = () => {
-  for (let i = 0; i <= 100000; i++) {
+  for (let i = 80001; i <= 100000; i++) {
     const name = faker.company.companyName();
     const imagePage = {
       id: i,
@@ -21,14 +21,14 @@ const seed = () => {
     };
 
     for (let j = 100; j > 0; j--) {
-      const num = Math.floor((Math.random() * 822 + 1));
-      const url = `https://hrr42-fec5.s3-us-west-1.amazonaws.com/photo${num}.jpg`;
+      const num = Math.floor((Math.random() * 328 + 1));
+      const url = `https://hrr42-sdc6.s3-us-west-1.amazonaws.com/photo/${num}.jpg`;
       imagePage.imageUrls.push(url);
     };
   records.push(imagePage);
 
 }
-  console.log(records);
+console.log(records);
 };
 
 seed();
@@ -41,5 +41,3 @@ csvWriter
   .catch((err) => {
     console.log(err)
   });
-
-
