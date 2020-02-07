98e4ed986042ddbba575155e1f7fe6374ea081f3
apple <apple@appletekiair.lan>
2020-02-06 23:48:01 -0800

    final version

--- server/index.js
+++ server/index.js
@@ -28,37 +28,6 @@ app.get('/api/images/:restaurantId', (req, res) => {
     });
 });
 
-// app.post('/api/images', (req, res) => {
-//   const data = req.body;
-//   db.insert(data)
-//     .then(res.status(201).end())
-//     .catch(error => {
-//       console.log('Error occurs in Post')
-//       res.status(404).end();
-//     });
-// });
-
-// app.put('/api/images/:restaurantId', (req, res) => {
-//   const data = req.body;
-//   const id = req.params.restaurantId;
-//   db.updateData(id, data)
-//     .then(res.status(201).end())
-//     .catch(error => {
-//       console.log('Error occurs in Put')
-//       res.status(400).end();
-//   });
-// });
-
-// app.delete('/api/images/:restaurantId', (req, res) => {
-//   const id = req.params.restaurantId;
-//   db.del(id)
-//     .then(res.status(201).end())
-//     .catch(error => {
-//       console.log('Error occurs in Delete')
-//       res.status(400).end();
-//   })
-// });
-
 const port = process.env.PORT || 9042;
 
 app.listen(port, console.log(`Server running on port: ${port}`));
